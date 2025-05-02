9db9d0ad73a30610971abd482f462105af76fd27
Austin <20157814+adurr21@users.noreply.github.com>
2025-05-01 21:28:45 -0400

    Updated Header to Show Name

--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -2,7 +2,8 @@ import { signOutAction } from "@/app/actions";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import Link from "next/link";
 import { Badge } from "./ui/badge";
-import { Button } from "./ui/button";
+//import { Button } from "./ui/button";
+import Button from '@mui/joy/Button'
 import { createClient } from "@/utils/supabase/server";
 
 export default async function AuthButton() {
@@ -12,57 +13,27 @@ export default async function AuthButton() {
     data: { user },
   } = await supabase.auth.getUser();
 
-  if (!hasEnvVars) {
-    return (
-      <>
-        <div className="flex gap-4 items-center">
-          <div>
-            <Badge
-              variant={"default"}
-              className="font-normal pointer-events-none"
-            >
-              Please update .env.local file with anon key and url
-            </Badge>
-          </div>
-          <div className="flex gap-2">
-            <Button
-              asChild
-              size="sm"
-              variant={"outline"}
-              disabled
-              className="opacity-75 cursor-none pointer-events-none"
-            >
-              <Link href="/sign-in">Sign in</Link>
-            </Button>
-            <Button
-              asChild
-              size="sm"
-              variant={"default"}
-              disabled
-              className="opacity-75 cursor-none pointer-events-none"
-            >
-              <Link href="/sign-up">Sign up</Link>
-            </Button>
-          </div>
-        </div>
-      </>
-    );
-  }
+  const { data: profile, error } = await supabase
+    .from('users')
+    .select('*')
+    .eq('id', user?.id)
+    .single();
+    
   return user ? (
     <div className="flex items-center gap-4">
-      Hey, {user.email}!
+      Hey, {profile.name}!
       <form action={signOutAction}>
-        <Button type="submit" variant={"outline"}>
+        <Button type="submit" variant="solid" color="primary">
           Sign out
         </Button>
       </form>
     </div>
   ) : (
     <div className="flex gap-2">
-      <Button asChild size="sm" variant={"outline"}>
+      <Button size="md" variant="solid">
         <Link href="/sign-in">Sign in</Link>
       </Button>
-      <Button asChild size="sm" variant={"default"}>
+      <Button size="md" variant="solid">
         <Link href="/sign-up">Sign up</Link>
       </Button>
     </div>
